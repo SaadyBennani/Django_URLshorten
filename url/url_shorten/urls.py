c19d2869ee551b8c9211702c05b6f2d4494c62e9
SaadyBennani <sbennani23@gmail.com>
2019-05-03 18:57:08 -0700

    shortify works

--- url/url_shorten/urls.py
+++ url/url_shorten/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-app_name = '<url_shorten'  # for namespacing
+app_name = 'url_shorten'  # for namespacing
 urlpatterns = [
     path('', views.index, name='index'),
-    path('shortened', views.shortened, name='short')
+    path('shortened', views.shortened, name='shortened')
 ]
--- url/url_shorten/views.py
+++ url/url_shorten/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
+import string
+from .models import URL
 
 
 # Create your views here.
@@ -8,5 +11,14 @@ def index(request):
 
 
 def shortened(request):
-    # long_url = request.POST.get['url']
-    return render(request, 'url_shorten/short.html')
+    if request.method == 'POST':
+        long_url = request.POST['url']
+        short_url = shortify()
+        url = URL(url_long=long_url, url_short=short_url)
+        url.save()
+        return render(request, 'url_shorten/short.html', {'short_url': short_url, 'long_url': url})
+
+
+def shortify(size=8):
+    chars = string.ascii_lowercase + string.digits
+    return ''.join(random.choice(chars) for _ in range(size))
